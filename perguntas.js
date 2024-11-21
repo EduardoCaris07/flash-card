ba44c68221223711b70bbfbf429c797acdb1b9b5
EduardoCaris07 <143615195+EduardoCaris07@users.noreply.github.com>
2024-11-21 18:17:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+
+criaCartao(
+    'Vôlei',
+    'Qual a altura da rede no vôlei masculino?',
+    'Altura 2,43m na categoria masculina '
+)
+
+criaCartao(
+    'Vôlei',
+    'Qual posição é responsável por levantar a bola pro ataque?',
+    'Levantador '
+)
+
+criaCartao(
+    'Vôlei',
+    'Um jogador pode fazer 2 toques seguidos?',
+    'Não é permitido'
+)
+
+criaCartao(
+    'Vôlei',
+    'Quantos sets tem um jogo de vôlei?',
+    '5 sets'
+)
+
+criaCartao(
+    'Vôlei',
+    'Qual a altura da rede no vôlei feminino',
+    'Altura 2,24m na categoria feminina'
+)
+
+criaCartao(
+    'Vôlei',
+    'Quantos jogadores fica na quadra durante jogo de vôlei?',
+    'São 6 jogadores dentro da quadra'
+)
+
+criaCartao(
+    'Vôlei',
+    'Quantos pontos tem que ser feito para acabar um set?',
+    '25 pontos, com 2 pontos de vantagem sobre o adversário'
+)
+
+criaCartao(
+    'Vôlei',
+    'Cada set tem quanto tempo de duração?',
+    'No vôlei não tem tempo de duração'
+)
